1df717909ed856b5419e96d7950fc251ff7186ef
Cole Callaway <nccallaway54@gmail.com>
2022-03-30 17:10:54 -0600

    First endpoint

--- server/index.js
+++ server/index.js
@@ -8,4 +8,9 @@ app.use(express.json());
 
 app.use(cors());
 
+app.get("/api/users", (req, res) => {
+  let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
+  res.status(200).send(friends);
+});
+
 app.listen(4000, () => console.log("Server running on port 4000"));
